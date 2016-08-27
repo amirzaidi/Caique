6c54962dfa60f4814154387582fb537471582e09
Amir Zaidi <azaidi@live.nl>
2016-08-27 15:51:01 +0200

    -

---
+++ app/src/main/java/v6/caique/MyFirebaseMessagingService.java
@@ -0,0 +1,11 @@
+package v6.caique;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
