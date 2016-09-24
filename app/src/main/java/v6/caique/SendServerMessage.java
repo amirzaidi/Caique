e3eb1dc0841b2d7e2c46b00412679b7327974231
kaniis <kaniis98@gmail.com>
2016-09-24 21:29:53 +0200

    Created multiple text fields whose values can be sent to the server

--- app/src/main/java/v6/caique/SendServerMessage.java
+++ app/src/main/java/v6/caique/SendServerMessage.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class SendServerMessage extends AppCompatActivity {
+
+    Boolean Error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +22,48 @@ public class SendServerMessage extends AppCompatActivity {
 
     public void SendMessageToServer()
     {
-        FirebaseMessaging fm = FirebaseMessaging.getInstance();
-        fm.send(new RemoteMessage.Builder(getString(R.string.gcm_defaultSenderId) + "@gcm.googleapis.com")
+        EditText Input = (EditText)findViewById(R.id.ChatId);
+        String ChatID = Input.getText().toString();
+
+        Input = (EditText)findViewById(R.id.SenderId);
+        String SenderID = Input.getText().toString();
+
+        Input = (EditText)findViewById(R.id.MessageType);
+        String MessageType = Input.getText().toString();
+
+        Input = (EditText)findViewById(R.id.Date);
+        String Date = Input.getText().toString();
+
+        Input = (EditText)findViewById(R.id.Text);
+        String Text = Input.getText().toString();
+
+        if(!ChatID.isEmpty() && !SenderID.isEmpty() && !Date.isEmpty()) {
+            FirebaseMessaging fm = FirebaseMessaging.getInstance();
+            fm.send(new RemoteMessage.Builder(getString(R.string.gcm_defaultSenderId) + "@gcm.googleapis.com")
                 .setMessageId(Integer.toString(MyFirebaseInstanceIDService.msgId.incrementAndGet()))
-                .addData("Heyo Captain Jack!", "Bring me back to the railroad track!")
-                .addData("Het werkt perfect", "en vlekkeloos")
+                .addData("chat", ChatID)
+                .addData("sender", SenderID)
+                .addData("type", MessageType)
+                .addData("date", Date)
+                .addData("text", Text)
                 .build());
 
-        Log.d("SendMessageToServer", "Message sent");
+            Log.d("SendMessageToServer", "Message sent");
+            Error = false;
+        }
+        else
+        {
+            Error = true;
+        }
     }
 
     public void SendMessage(View view){
 
         SendMessageToServer();
-        this.finish();
+        if(!Error)
+        {
+            this.finish();
+        }
 
     }
 }
