1da106b51cf21fb421aa72a29bfa962ba9920d30
Ismet <iismet__@live.nl>
2016-11-23 22:05:32 +0100

    Commit test 4 ofzo...

--- app/src/main/java/v6/caique/MainActivity.java
+++ app/src/main/java/v6/caique/MainActivity.java
@@ -171,17 +171,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_chats) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_contacts) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_add_people) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_music_library) {
+
+        } else if (id == R.id.nav_search) {
 
         }
 
